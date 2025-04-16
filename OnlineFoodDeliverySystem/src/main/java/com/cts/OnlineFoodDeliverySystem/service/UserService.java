50fc83bb9187e2b1df6082d96df4b0e2a2f69651
Preethi331 <Preethi.Angadi@cognizant.com>
2025-04-16 16:17:23 +0530

    Add files via upload

---
+++ OnlineFoodDeliverySystem/src/main/java/com/cts/OnlineFoodDeliverySystem/service/UserService.java
@@ -0,0 +1,8 @@
+package com.cts.OnlineFoodDeliverySystem.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
---
+++ OnlineFoodDeliverySystem/src/test/java/com/cts/OnlineFoodDeliverySystem/OnlineFoodDeliverySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.OnlineFoodDeliverySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodDeliverySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
