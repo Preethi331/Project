2e5d88ee2434d096f9cece23c63bfaaf3eb0595a
Dhanush12028 <Dhanush.Sivaprakasam@cognizant.com>
2025-04-17 17:11:29 +0530

    Delete OnlineFoodDeliverySystem directory

--- OnlineFoodDeliverySystem/src/main/java/com/cts/OnlineFoodDeliverySystem/controller/CustomerController.java
+++
@@ -1,8 +0,0 @@
-package com.cts.OnlineFoodDeliverySystem.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class CustomerController {
-
-}
--- OnlineFoodDeliverySystem/src/main/java/com/cts/OnlineFoodDeliverySystem/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.cts.OnlineFoodDeliverySystem.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-
-}
